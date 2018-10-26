d7e0e58b8c79ff70f92a6df0d76388959da23293
Yuchen Lu <luyuchen.paul@gmail.com>
2018-10-26 07:16:05 -0400

    separate data utility

---
+++ ld_research/text/__init__.py
@@ -0,0 +1,4 @@
+"""
+    Created by yuchen on 10/26/18
+    Description:
+"""
---
+++ ld_research/text/datasets.py
@@ -0,0 +1,13 @@
+""" Contains object for dataset and vocab
+"""
+from torch.utils.data import Dataset
+
+class Vocab:
+    pass
+
+class IWSLTDataset(Dataset):
+    """ An object for dataset """
+    def __len__(self):
+        pass
+    def __getitem__(self, item):
+        pass
