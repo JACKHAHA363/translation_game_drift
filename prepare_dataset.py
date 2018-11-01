d7755b5d9acfab742f569a04874e49402d7bcc95
Yuchen Lu <luyuchen.paul@gmail.com>
2018-11-01 16:11:47 -0400

    modify preprocess

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,7 +1,7 @@
 """ prepare the ISWIT and Multi30k task1 dataset.
 """
 from ld_research.settings import FR, EN, DE
-from ld_research.text.utils import prepare_IWSLT, prepare_multi30k, learn_bpe, apply_bpe_iwslt, apply_bpe_multi30k
+from ld_research.text.preprocess import prepare_IWSLT, prepare_multi30k, learn_bpe, apply_bpe_iwslt, apply_bpe_multi30k
 
 if __name__ == '__main__':
     """ Main logic """
